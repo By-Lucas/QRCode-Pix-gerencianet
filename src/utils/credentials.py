022591faac2e9819d2a6f3eb41852525019f7682
Lucas Silva <83965969+By-Lucas@users.noreply.github.com>
2022-09-04 19:50:20 -0300

    Update credentials.py

--- src/utils/credentials.py
+++ src/utils/credentials.py
@@ -1,21 +1,21 @@
-client_id = "Client_Id_d731ae830f8e0f308603fffea3a8a748436e946a"
-client_secret = "Client_Secret_b99dccf75c043fe2a47518dc02561d8d9b032aef"
+client_id = "Client_Id_d731ae830f8e0f30" # Adicione aqui sua CLIENT_ID da gerencianet
+client_secret = "Client_Secret_b99dccf75c043fe2a4" # Adicione aqui sua CLIENT_SECRET da gerencianet
 certificado = 'certificados/certificado-producao.pem'
 sandbox = True
 
-# CREDENCIAIS HOMOLOGACAO
-client_id_homologacao = 'Client_Id_493944299db401abb42a0251b0e28cf0eda2a507'
-client_secret_homologacao = 'Client_Secret_22e044074c3c82f4bf95400756f323ed7608907c'
+# CREDENCIAIS SE SEU CERTIFICADO FOR DE HOMOLOGACAO
+client_id_homologacao = 'Client_Id_493944299db401abb4' # Adicione aqui sua CLIENT_ID da gerencianet
+client_secret_homologacao = 'Client_Secret_22e044074c3c8'  # Adicione aqui sua CLIENT_SECRET da gerencianet
 certificado_homologacao = 'certificados/certificado-homologacao.pem'
 
-chave_pix = '65906cbf-abea-4fe1-ad2a-c0e1e719f36b'
+chave_pix = '65906cbf-abea-4fe1-ad2a-c0e1e719f36b' # COLOQUE SUA CHAVE PIX, CRIADA PELO APP DA GERENCIANET
        
 # encoding: utf-8
 
 # Put your app credentials here
 CREDENTIALS = {
-    'client_id': 'Client_Id_d731ae830f8e0f308603fffea3a8a748436e946a',
-    'client_secret': 'Client_Secret_b99dccf75c043fe2a47518dc02561d8d9b032aef',
+    'client_id': 'Client_Id_d731ae830f8e0', # Adicione aqui sua CLIENT_ID da gerencianet
+    'client_secret': 'Client_Secret_b99dccf75', # Adicione aqui sua CLIENT_SECRET da gerencianet
     'sandbox': False,
     'pix_cert': 'certificados/certificado-producao.pem'
 }
